e453e1e0f8168d96bc63f8f66e460de0c16852db
SaminaMahjabeen <samina.hccd@gmail.com>
2020-11-02 00:32:09 +0600

    first commit

---
+++ letconst.js
@@ -0,0 +1,20 @@
+const h="sajib"
+console.log(h)
+const num=[12,34];
+num[1]=13;
+num.push(11);
+console.log(num);
+const a={name:"samina mahjabeen",r:"ty"};
+
+console.log(a);
+let  sum=0;
+for(let i=0;i<=3;i++){
+sum=sum+i;
+}
+function add(num1,num2){
+    num2=num2||1;
+    return num1+num2;
+
+}
+const total=add(12);
+console.log(total);
